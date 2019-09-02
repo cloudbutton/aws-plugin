e04202d4e378af388cda00400662dcbafbdb06e0
Aitor Arjona <aitor.a98@gmail.com>
2019-09-02 12:26:29 +0200

    Fix in installation script

--- install.py
+++ install.py
@@ -2,11 +2,8 @@ import pywren_ibm_cloud
 import os
 import shutil
 
-path_list = os.path.abspath(pywren_ibm_cloud.__file__).split('/')[:-1]
-base_path = '/'.join(path_list)
-
-path_list = os.path.abspath(__file__).split('/')[:-1]
-source_path = '/'.join(path_list)
+base_path = os.path.dirname(pywren_ibm_cloud.__file__)
+source_path = os.path.dirname(__file__)
 
 aws_cb_backend_path = os.path.join(os.path.join(base_path, 'compute', 'backends'), 'aws_lambda')
 aws_s3_backend_path = os.path.join(os.path.join(base_path, 'storage', 'backends'), 'aws_lambda')
